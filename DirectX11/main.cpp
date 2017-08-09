75d79d11e0c43499c205a59ab470dc403652c665
Luca Mazza <mazza.luca@gmail.com>
2017-08-09 15:33:09 +0200

    1_3

--- DirectX11/main.cpp
+++ DirectX11/main.cpp
@@ -83,6 +83,25 @@ void callTestDirectXMath2()
 	cout << "-------------------------" << endl;
 }
 
+void callFloatingPointError()
+{
+	cout << "Chapter 1 example 3" << endl;
+	streamsize ss = cout.precision();
+	cout.precision(8);
+	XMVECTOR u = XMVectorSet(1.0f, 1.0f, 1.0f, 0.0f);
+	XMVECTOR n = XMVector3Normalize(u);
+	float LU = XMVectorGetX(XMVector3Length(n));
+	// Mathematically, the length should be 1.
+	cout << LU << endl;
+	if (LU == 1.0f) cout << "Length 1" << endl;
+	else cout << "Length not 1" << endl;
+	// Raising 1 to any power should still be 1
+	float powLU = powf(LU, 1.0e6f);
+	cout << "LU^(10^6) = " << powLU << endl;
+	cout.precision(ss);
+	cout << "-------------------------" << endl;
+}
+
 int main()
 {
 	cout.setf(ios_base::boolalpha);
@@ -95,6 +114,7 @@ int main()
 
 	callTestDirectXMath();
 	callTestDirectXMath2();
+	callFloatingPointError();
 	
 	system("pause");
 	return 0;
